0e0ae2be8cf7b5c5939517a4a27af20ed2de4bb6
skyler <ho971122@gmail.com>
2023-03-08 11:03:10 +0900

    #2 HoLee / 프로그래머스 / Level.0 / 종이자르기

---
+++ Programmers/Level.0/종이자르기/Skyler.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ M:Int, _ N:Int) -> Int { (M - 1) + ((N - 1) * M) }
+
+//func solution(_ M:Int, _ N:Int) -> Int {
+//    if M == 0 && N == 0 {
+//        return 0
+//    }
+//
+//    return (M * N) - 1
+//}
