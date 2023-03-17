3d19423fb694e5d010b2c26b64fdbe6e0c98d3c6
skyler <ho971122@gmail.com>
2023-03-17 14:07:39 +0900

    #2 Ho / 프로그래머스 / Level.0 / 연속된수의합

---
+++ Programmers/Level.0/연속된수의합/Ho.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func solution(_ num:Int, _ total:Int) -> [Int] {
+    var answer = [Int]()
+    var re = total/num
+    var n = num/2
+    
+    if num%2 == 0{
+        answer = Array(re-n+1...re+n)
+    }else{
+        answer = Array(re-n...re+n)
+    }
+    return answer
+}
+
+//func solution(_ num:Int, _ total:Int) -> [Int] {
+//    let base = (total - (num * (num + 1) / 2)) / num + 1
+//    return (0..<num).map { $0 + base }
+//}
