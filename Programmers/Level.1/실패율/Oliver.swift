594d5dbb14193d6b4c58908fbc3da67f148a2f19
Oliver03710 <jhee2619@gmail.com>
2023-03-17 23:41:09 +0900

    #1 Oliver / 프로그래머스 / Level.1 / 실패율

---
+++ Programmers/Level.1/실패율/Oliver.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var failure = [Int: Double]()
+    var total = Double(stages.count)
+    var countArr = Array(repeating: 0, count: N+2)
+    
+    for num in stages {
+        countArr[num] += 1
+    }
+    
+    for i in 1..<N+1 {
+        if countArr[i] == 0 {
+            failure[i] = 0.0
+        } else {
+            total = total - Double(countArr[i])
+            failure[i] = Double(countArr[i]) / total
+        }
+    }
+    let sortArr = failure.sorted(by: <).sorted(by: { $0.1 > $1.1 })
+    let result = sortArr.map{ $0.key }
+    
+    return result
+}
